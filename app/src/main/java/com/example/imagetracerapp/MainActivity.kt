fe2fa0bf210aba950c840d846cf468865fd82345
Ashhal Wallani <wallani.ash06@gmail.com>
2026-01-12 23:24:47 -0500

    Added new feature where user can use the slider at to change the transparnecy of the image

--- app/src/main/java/com/example/imagetracerapp/MainActivity.kt
+++ app/src/main/java/com/example/imagetracerapp/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.imagetracerapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var overlayImage: ImageView
+    private lateinit var opacitySlider: SeekBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        overlayImage = findViewById(R.id.overlayImage)
+        opacitySlider = findViewById(R.id.opacitySlider)
+
+        // temporary image for testing
+        overlayImage.setImageResource(android.R.drawable.ic_menu_gallery)
+
+        opacitySlider.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(
+                seekBar: SeekBar?,
+                progress: Int,
+                fromUser: Boolean
+            ) {
+                overlayImage.alpha = progress / 100f
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
 }
